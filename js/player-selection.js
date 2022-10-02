1d332003094503ac9c161bca55863ccc8d23d7cf
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-02 13:51:55 +0600

    button disable done

--- js/player-selection.js
+++ js/player-selection.js
@@ -1,9 +1,21 @@
+function buttonDisabled(buttonId){
+    const playerSelectionButton = document.getElementById(buttonId);
+    playerSelectionButton.disabled = true;
+}
+
 function playerNameSelection(playerId){
     const playerNameValue = document.getElementById(playerId);
     const playerName = playerNameValue.innerText;
 
     const playersList = document.getElementById('players-list');
-    let newPlayerName = document.createElement('li');
-    newPlayerName.textContent = playerName;
-    playersList.appendChild(newPlayerName);
+
+    if(playersList.childElementCount <= 4){
+        let newPlayerName = document.createElement('li');
+        newPlayerName.textContent = playerName;
+        playersList.appendChild(newPlayerName);
+    }
+    else{
+        alert('You have selected the best five players');
+    }
+    
 }
